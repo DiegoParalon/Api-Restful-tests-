9d0306a73329dccd577d5ddbc6a2dc0d3369f51d
Diego Paralon <diegoparalon@hotmail.com>
2023-11-15 18:39:22 +0000

    Method put

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -32,7 +32,35 @@ async function post(req, res) {
 
 }
 
+async function put(req,res) {
+    const { id } = req.params
+    const product = await ProductsModels.findOneAndUpdate({ _id: id }, req.body, {new: true} )
+
+    res.send({
+        message:'succes',
+        product,
+    })
+
+
+
+
+  /*
+   const product = await ProductsModels.findOne({ _id: id })
+
+    await product.updateOne(req.body)
+
+    res.send({
+        message:'sucess',
+        product,
+    })
+*/
+
+
+
+}
+
 module.exports = {
     get,
     post,
+    put,
 }
