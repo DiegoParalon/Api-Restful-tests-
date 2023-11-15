ce64db272de881193ca8d9ab4558252e7918058a
Diego Paralon <diegoparalon@hotmail.com>
2023-11-15 19:39:34 +0000

    cross, permitindo ou negando conexoes com a api

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
-//const path = require('path')
+const cors = require('cors')
+
 
 const db = require('./database/db')
 const routes = require('./routes/routes')
@@ -7,6 +8,26 @@ const routes = require('./routes/routes')
 const app = express()
 
 db.connect()
+
+const allowedOrigins = [
+    'http://127.0.0.1:5501',
+    'www.teste.pt',
+]
+
+//habilita cors 
+app.use(cors({
+    origin: function(origin, callback) {
+
+        let allowed = true
+
+        if (!origin) allowed = true 
+
+        if (!allowedOrigins.includes(origin)) allowed = false 
+
+        callback(null, allowed)
+    }
+}))
+
 
 app.use(express.json())
 
